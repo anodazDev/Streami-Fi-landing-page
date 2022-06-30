214b2e6342fc1b5ef7c5af556e62b7e42112e429
Pakdeepum <31123616+Pakdeepum@users.noreply.github.com>
2022-06-30 22:14:08 +0700

    streami

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.scss'
 import '../styles/index.css'
+import '../styles/streami.scss'
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
+
+
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-in",
+      once: true,
+      duration: 500,
+    });
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
