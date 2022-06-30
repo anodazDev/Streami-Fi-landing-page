2c3cc565dce83572ff08cc558bd4d1339a73dc39
twistseason <39827654+twistseason@users.noreply.github.com>
2022-06-30 11:20:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function index() {
   return (
     <>
-      <h1 className=" text-primary"> 
+      <h1 className=" text-primary !text-xl md:!text-2xl xl:!text-4xl"> 
         Hello world!
       </h1>
     </>
