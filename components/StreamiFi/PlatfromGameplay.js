1f20ec157677661c9d89c550d5477481b34a3659
Pakdeepum <31123616+Pakdeepum@users.noreply.github.com>
2022-07-01 14:36:22 +0700

    Gameplay

--- components/StreamiFi/PlatfromGameplay.js
+++ components/StreamiFi/PlatfromGameplay.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 import PlatfromBoxImage from '../../public/media/img/PlatfromBox.svg'
 import GameplayBoxImage from '../../public/media/img/GameplayBox.svg'
+import GameplayArrowImage from '../../public/media/img/GameplayArrow.svg'
 import Styles from '../../styles/components/StreamiFi/PlatfromGameplay.module.scss'
 
 export default function index() {
   return (
     <>
     <div className={`${Styles.bg_gradient_dark} ${Styles.bottom_triangle_gradient} ${'flex'}`}>
-      <div className={`${'container mx-auto'}`}>
-        <div className={`${Styles.layout_M} ${Styles.mb_10rem}`}>
-            <div className={`${Styles.image_x_auto} ${'container flex'}`} data-aos="fade-in">
+      <div className={`${'mx-auto w-full'}`}>
+        <div className={`${Styles.layout_M} ${Styles.mb_8rem}`}>
+            <div className={`${Styles.image_x_auto} ${'container flex mx-auto'}`} data-aos="fade-in">
                 <Image
                     src={PlatfromBoxImage}
                     alt="Picture of the author"
@@ -19,21 +20,35 @@ export default function index() {
                     className="mx-auto"
                     />
             </div>
-            <div className={`${Styles.text_center} ${'container mx-auto'}`} data-aos="zoom-out" data-aos-delay="200">
+            <div className={`${Styles.text_center} ${'container mx-auto'}`} data-aos="zoom-out" data-aos-delay="400">
                 <button className={`${Styles.btn_pill_10} ${Styles.btn_border_white} ${Styles.text_white} ${'btn py-2 px-4'}`}>PLATFROM</button>
             </div>
         </div>
-        <div className={`${Styles.layout_M} ${Styles.mb_10rem}`}>
-            <div className={`${Styles.image_x_auto} ${'container flex'}`}>
+        <div className={`${'relative'}`}>
+          <div className={`${Styles.layout_M} ${Styles.mb_10rem}`}>
+              <div className={`${Styles.image_x_auto} ${'container flex mx-auto'}`}>
+                  <Image
+                      src={GameplayBoxImage}
+                      alt="Picture of the author"
+                      width="700"
+                      height="350"
+                      className={`${'mx-auto'}`}
+                      />
+              </div>
+          </div>
+          <div className={`${Styles.gameplay_animattion_box}`}>
+              <div className={`${'absolute'}`}>
                 <Image
-                    src={GameplayBoxImage}
-                    alt="Picture of the author"
-                    width="700"
-                    height="350"
-                    className={`${'mx-auto'}`}
-                    />
-            </div>
+                  src={GameplayArrowImage}
+                  alt="Picture of the author"
+                  width="100"
+                  height="100"
+                  className={`${'mx-auto'}`}
+                  />
+              </div>
+          </div>
         </div>
+
       </div>
     </div>
     </>
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import '../styles/index.css'
 import AOS from "aos";
 import { useEffect } from "react";
+import Head from 'next/head'
 import "aos/dist/aos.css";
 
 
@@ -13,11 +14,15 @@ function MyApp({ Component, pageProps }) {
       easing: "ease-in",
       once: true,
       duration: 800,
+      delay: 200,
     });
   }, []);
 
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </>
 
