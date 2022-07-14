0a18b8da9ae3aa35eefc3c0f908bf7dedb0d63fd
twistseason <39827654+twistseason@users.noreply.github.com>
2022-07-14 23:52:39 +0700

    responsive

--- components/TokenomicRoadmap.js
+++ components/TokenomicRoadmap.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import TokenomicChartImage from '../public/media/img/Tokenomic/TokenomicChart.svg'
+import TokenomicChartImage from '../public/media/img/Tokenomic/TokenomicChart.png'
 import TokenomicLogoComponent from '../public/media/img/Tokenomic/Logo_round.png'
 import TokenomicBlurLogoComponent from '../public/media/img/Tokenomic/Logo_round_blur.png'
 import RoadmapImage from '../public/media/img/Roadmap/Roadmap.svg'
@@ -9,121 +9,121 @@ import Styles from '../styles/components/TokenomicRoadmap.module.scss'
 
 export default function index() {
     return (
-    <>
-    <div className={`${Styles.bg_gradient_dark} ${Styles.bottom_triangle_gradient} ${'flex'}`}>
-        <div className={`${'mx-auto w-full'}`}>
-            {/* Tokenomic */}
-            <div className={`${'relative flex flex-col mt-16'}`}>
-                {/* Tokenomic Main Box*/}
-                <div className={`${Styles.layout_M} ${'!mb-32'} `}>
-                    {/* Tokenomic title */}
-                    <div className={`${'container mx-auto mb-8 text-center'}`} data-aos="zoom-out">
-                        <label className={`${Styles.font_header} ${'font-semibold text-white'}`}>TOKENOMIC</label>
-                    </div>
-                    {/* Tokenomic Image */}
-                    <div className={` ${' mx-auto'}`}>
-                        <div className={`${Styles.image_x_auto} ${'container flex mx-auto mb-8'}`} data-aos="fade-in">
-                            <Image
-                                src={TokenomicChartImage}
-                                alt="Picture of the author"
-                                width="700"
-                                height="350"
-                                className="mx-auto"
+        <>
+            <div className={`${Styles.bg_gradient_dark} ${Styles.bottom_triangle_gradient} ${'flex'}`}>
+                <div className={`${'mx-auto w-full'}`}>
+                    {/* Tokenomic */}
+                    <div className={`${'relative flex flex-col mb-16'}`}>
+                        {/* Tokenomic Main Box*/}
+                        <div className={`${Styles.layout_L} ${''} `}>
+                            {/* Tokenomic title */}
+                            <div className={`${'container mx-auto mb-8 text-center'}`} data-aos="zoom-out">
+                                <label className={`${Styles.font_header} ${'font-semibold text-white'}`}>TOKENOMIC</label>
+                            </div>
+                            {/* Tokenomic Image */}
+                            <div className={` ${' mx-auto'}`}>
+                                <div className={`${Styles.image_x_auto} ${'container flex mx-auto mb-8'}`} data-aos="fade-in">
+                                    <Image
+                                        src={TokenomicChartImage}
+                                        alt="Picture of the author"
+                                        width="700"
+                                        height="350"
+                                        className="mx-auto"
+                                    />
+                                </div>
+                            </div>
+                            {/* Tokenomic decsription */}
+                            <div className={`${'container mx-auto text-center'}`} data-aos="fade-in">
+                                <label className={`${Styles.font_sub_header} ${'font-semibold text-white'}`}>Total amount 1000000</label>
+                            </div>
+                            {/* top left */}
+                            <div className={`${'absolute right-[32%] top-[11%]'} ${'hidden xl:block'}`} data-aos="fade-up" data-aos-delay="200">
+                                <Image
+                                    src={TokenomicBlurLogoComponent}
+                                    alt="Picture of the author"
+                                    width="40"
+                                    height="40"
                                 />
+                            </div>
+                            {/* bottom right */}
+                            <div className={`${'absolute left-[33%] top-[82%]'}  ${'hidden xl:block'}`} data-aos="fade-down" data-aos-delay="200">
+                                <Image
+                                    src={TokenomicBlurLogoComponent}
+                                    alt="Picture of the author"
+                                    width="40"
+                                    height="40"
+                                />
+                            </div>
+                        </div>
+                        {/* Tokenomic Compenent Box*/}
+                        <div className={`${'absolute w-full h-full top-0 -z-10'}`}>
+                            {/* top left */}
+                            <div className={`${'absolute left-[-65px] top-[20%]'} ${'!opacity-25 xl:!opacity-100'}`} data-aos="fade-up" data-aos-delay="200">
+                                <Image
+                                    src={TokenomicLogoComponent}
+                                    alt="Picture of the author"
+                                    width="130"
+                                    height="130"
+                                />
+                            </div>
+                            {/* top right */}
+                            <div className={`${'absolute right-[-70px] top-[15%]'} ${'!opacity-25 xl:!opacity-100'}`} data-aos="fade-down" data-aos-delay="300">
+                                <Image
+                                    src={TokenomicLogoComponent}
+                                    alt="Picture of the author"
+                                    width="140"
+                                    height="140"
+                                />
+                            </div>
+                            {/* bottom left */}
+                            <div className={`${'absolute left-[15%] top-[65%]'} ${'!opacity-25 xl:!opacity-100'}`} data-aos="fade-down" data-aos-delay="400">
+                                <Image
+                                    src={TokenomicLogoComponent}
+                                    alt="Picture of the author"
+                                    width="80"
+                                    height="80"
+                                />
+                            </div>
+                            {/* bottom right */}
+                            <div className={`${'absolute right-[15%] top-[55%]'} ${'!opacity-25 xl:!opacity-100'}`} data-aos="fade-down" data-aos-delay="500">
+                                <Image
+                                    src={TokenomicLogoComponent}
+                                    alt="Picture of the author"
+                                    width="80"
+                                    height="80"
+                                />
+                            </div>
                         </div>
                     </div>
-                    {/* Tokenomic decsription */}
-                    <div className={`${'container mx-auto text-center'}`} data-aos="fade-in">
-                        <label className={`${Styles.font_sub_header} ${'font-semibold text-white'}`}>Total amount 1000000</label>
+                    {/* Roadmap */}
+                    <div className={`${'relative mb-[20] z-10'}`}>
+                        {/* Roadmap Image */}
+                        <div className={`${Styles.image_x_auto} ${'w-full flex mx-auto'}`} data-aos="fade-in">
+                            {/* Roadmap title */}
+                            <div className={`${'mx-auto text-right absolute '} ${' -top-8 sm:-top-0 left-8 md:left-[15%] xl:left-[20%]'}`} data-aos="zoom-out">
+                                <label className={`${'font-semibold text-white leading-none '} ${'text-2xl md:text-4xl xl:text-[56px]'}`}>ROAD</label>
+                                <br />
+                                <label className={`${'font-semibold  leading-none  text-[#CB4297]'} ${'text-2xl md:text-4xl xl:text-[56px]'}`}>MAP</label>
+                            </div>
+                            <Image
+                                src={RoadmapImage}
+                                alt="Picture of the author"
+                                style={{ width: "100%" }}
+                                className="mx-auto"
+                            />
+                        </div>
                     </div>
-                    {/* top left */}
-                    <div className={`${'absolute right-[32%] top-[11%]'}`} data-aos="fade-up" data-aos-delay="200">
+                    {/* Gradient */}
+                    <div className={`${'absolute z-0 bottom-0'}`}>
                         <Image
-                        src={TokenomicBlurLogoComponent}
-                        alt="Picture of the author"
-                        width="40"
-                        height="40"
-                        />
-                    </div>
-                    {/* bottom right */}
-                    <div className={`${'absolute left-[33%] top-[82%]'}`} data-aos="fade-down" data-aos-delay="200">
-                        <Image
-                        src={TokenomicBlurLogoComponent}
-                        alt="Picture of the author"
-                        width="40"
-                        height="40"
-                        />
-                    </div>
-                </div>
-                {/* Tokenomic Compenent Box*/}
-                <div className={`${'absolute w-full h-full top-0 -z-10'}`}>
-                    {/* top left */}
-                    <div className={`${'absolute left-[-65px] top-[20%]'}`} data-aos="fade-up" data-aos-delay="200">
-                        <Image
-                        src={TokenomicLogoComponent}
-                        alt="Picture of the author"
-                        width="130"
-                        height="130"
-                        />
-                    </div>
-                    {/* top right */}
-                    <div className={`${'absolute right-[-70px] top-[15%]'}`} data-aos="fade-down" data-aos-delay="300">
-                        <Image
-                        src={TokenomicLogoComponent}
-                        alt="Picture of the author"
-                        width="140"
-                        height="140"
-                        />
-                    </div>
-                    {/* bottom left */}
-                    <div className={`${'absolute left-[15%] top-[65%]'}`} data-aos="fade-down" data-aos-delay="400">
-                        <Image
-                        src={TokenomicLogoComponent}
-                        alt="Picture of the author"
-                        width="80"
-                        height="80"
-                        />
-                    </div>
-                    {/* bottom right */}
-                    <div className={`${'absolute right-[15%] top-[55%]'}`} data-aos="fade-down" data-aos-delay="500">
-                        <Image
-                        src={TokenomicLogoComponent}
-                        alt="Picture of the author"
-                        width="80"
-                        height="80"
+                            src={GradientImage}
+                            alt="Picture of the author"
+                            style={{ width: "100%" }}
+                            className="mx-auto"
                         />
                     </div>
                 </div>
             </div>
-            {/* Roadmap */}
-            <div className={`${'relative mb-[20] z-10'}`}>
-                {/* Roadmap Image */}
-                <div className={`${Styles.image_x_auto} ${'w-full flex mx-auto'}`} data-aos="fade-in">
-                    {/* Roadmap title */}
-                    <div className={`${'mx-auto text-right absolute ml-[25%]'}`} data-aos="zoom-out">
-                        <label className={`${'font-semibold text-white leading-none text-[45px]'}`}>ROAD</label>
-                        <br/>
-                        <label className={`${'font-semibold text-white leading-none text-[45px] text-[#CB4297]'}`}>MAP</label>
-                    </div>
-                    <Image
-                        src={RoadmapImage}
-                        alt="Picture of the author"
-                        style={{ width: "100%" }}
-                        className="mx-auto"
-                        />
-                </div>
-            </div>
-            {/* Gradient */}
-            <div className={`${'absolute z-0 bottom-0'}`}>
-                <Image
-                    src={GradientImage}
-                    alt="Picture of the author"
-                    style={{ width: "100%" }}
-                    className="mx-auto"
-                    />
-            </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
--- components/case.js
+++ components/case.js
@@ -5,22 +5,30 @@ import Styles from '../styles/components/case.module.scss'
 export default function Case() {
     return (
         <>
-            <div className={`${Styles.overlay} ${' bg-gradient-to-br from-primary-300 to-supplement-500'}`}>
-                <div className={`${Styles.layout_L} ${' flex  flex-wrap '}`}>
-                    <div className={`${'w-full flex justify-end relative'}`}>
+            <div className={`${Styles.overlay} ${' bg-gradient-to-br from-primary-300 to-supplement-500 flex align-bottom'} ${'min-h-auto 2xl:min-h-[640px]'}`}>
+                <div className={` ${' flex  flex-wrap '}`}>
+                    <div className={`${' mx-[4%] w-full flex justify-end relative'}`}>
                         <div className={`${' w-fit px-5 py-2 justify-center items-center bg-black rounded-[9999px] absolute bottom-1/2 translate-y-1/2'}`}>
                             <h2 className={`${'text-white'}`}>ASSETUSE CASE</h2>
                         </div>
                     </div>
-                    <div className={`${'  items-center justify-center relative z-[2] '} ${' hidden sm:flex sm:w-4/12 xl:w-6/12'}`}>
+                    <div className={`${'  items-center justify-center relative z-[2] '} ${' hidden lg:flex w-full lg:w-4/12 '}`}>
                         <img
+                            data-aos="fade-right"
+                            data-aos-delay="200"
                             className={`${'img-fluid w-full object-bottom absolute bottom-0'}`}
                             src='../media/img/Case/Headphone.png'
                         />
                     </div>
-                    <div className={`${'  mt-14 flex flex-wrap p-2'} ${'w-full sm:w-8/12 xl:w-6/12'}`}>
+                    <div className={`${'  mt-14 flex flex-wrap p-2'} ${'w-full lg:w-8/12 '}`}>
                         <div className={`${'flex flex-wrap   p-2'} ${'w-full sm:w-1/2'}`}>
-                            <div className={`${'w-full border-2 border-white rounded-xl p-4'}`}>
+                            <div className={`
+                                ${'w-full border-2 border-white rounded-xl p-4'} 
+                                ${'bg-black/[0.25] sm:bg-transparent'}
+                            `}
+                                data-aos="fade-down"
+                                data-aos-delay="200"
+                            >
                                 <div className={`${'border-b-2 border-white w-3/4 mb-4'}`}>
                                     <h4 className={`${'text-white  break-normal w-[125%]'}`}>
                                         Year of Purchase
@@ -32,7 +40,13 @@ export default function Case() {
                             </div>
                         </div>
                         <div className={`${' flex  flex-wrap p-2'} ${'w-full sm:w-1/2'}`}>
-                            <div className={`${'w-full border-2 border-white rounded-xl p-4'}`}>
+                            <div className={`
+                                ${'w-full border-2 border-white rounded-xl p-4'} 
+                                ${'bg-black/[0.25] sm:bg-transparent'}
+                            `}
+                                data-aos="fade-down"
+                                data-aos-delay="400"
+                            >
                                 <div className={`${'border-b-2 border-white w-3/4 mb-4'}`}>
                                     <h4 className={`${'text-white  break-normal w-[125%]'}`}>
                                         Streami-fi Headphones
@@ -50,7 +64,13 @@ export default function Case() {
                             </div>
                         </div>
                         <div className={`${'flex flex-wrap p-2'} ${'w-full sm:w-1/2'}`}>
-                            <div className={`${'w-full border-2 border-white rounded-xl p-4'}`}>
+                            <div className={`
+                                ${'w-full border-2 border-white rounded-xl p-4'} 
+                                ${'bg-black/[0.25] sm:bg-transparent'}
+                            `}
+                                data-aos="fade-down"
+                                data-aos-delay="600"
+                            >
                                 <div className={`${'border-b-2 border-white w-3/4 mb-4'}`}>
                                     <h4 className={`${'text-white  break-normal w-[125%]'}`}>
                                         Limited Artist Edition
@@ -66,7 +86,13 @@ export default function Case() {
                             </div>
                         </div>
                         <div className={`${'flex flex-wrap  p-2'} ${'w-full sm:w-1/2'}`}>
-                            <div className={`${'w-full border-2 border-white rounded-xl p-4'}`}>
+                            <div className={`
+                                ${'w-full border-2 border-white rounded-xl p-4'} 
+                                ${'bg-black/[0.25] sm:bg-transparent'}
+                            `}
+                                data-aos="fade-down"
+                                data-aos-delay="800"
+                            >
                                 <div className={`${'border-b-2 border-white w-3/4 mb-4'}`}>
                                     <h4 className={`${'text-white  break-normal w-[125%]'}`}>
                                         Gift
